e8ae9e626195f11b550dd5e027eb7929a6ad1bdc
Akshat Tyagi <akshat.tyagi1909@gmail.com>
2026-08-25 10:12:44 +0530

    fix: bundle styles in production build

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import GameInitializer from './gameInitializer.js';
 import renderBoard from './DOM/render/renderBoard.js';
 import renderShipSelection from './DOM/render/renderShipSelection.js';
-import "./DOM/attackController.js"
-import "./DOM/placementController.js"
+import './DOM/attackController.js';
+import './DOM/placementController.js';
+import './styles.css';
 
 const gameInitializer = new GameInitializer();
 const playerGrid = document.querySelector('.set-player-grid');
